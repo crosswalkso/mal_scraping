a12fa875fe28d30408940992f483d7083ad3f6e5
crosswalk <crosswalk0628@gmail.com>
2023-03-20 00:48:43 +0900

    fetch branch start

--- scraper/frontend_mal/src/routes/Animes.jsx
+++ scraper/frontend_mal/src/routes/Animes.jsx
@@ -3,6 +3,6 @@ import { useEffect } from "react";
 export default function Animes() {
   useEffect(() => {
     fetch("http://127.0.0.1:8000/api/v1/seasons/1/animes");
-  });
+  }, []);
   return <h1>Animes</h1>;
 }
