1fa114061c5e358e894db6f874fe7f3364665bce
crosswalk <crosswalk0628@gmail.com>
2023-03-25 23:08:06 +0900

    urls_v1.0

--- scraper/backend_mal/config/urls.py
+++ scraper/backend_mal/config/urls.py
@@ -18,7 +18,12 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/animes/", include("animes.urls")),
     path("api/v1/seasons/", include("seasons.urls")),
     path("api/v1/genres/", include("genres.urls")),
     path("api/v1/broadcasts/", include("broadcasts.urls")),
+    path("api/v1/demos/", include("demos.urls")),
+    path("api/v1/sources/", include("sources.urls")),
+    path("api/v1/studios/", include("studios.urls")),
+    path("api/v1/themes/", include("themes.urls")),
 ]
