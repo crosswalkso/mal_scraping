e5f441f8f0d5f38541101b125b2bf85da3db63ae
crosswalk <crosswalk0628@gmail.com>
2023-04-24 14:51:41 +0900

    router modified

--- scraper/frontend_mal/src/router.jsx
+++ scraper/frontend_mal/src/router.jsx
@@ -1,9 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./routes/Home";
 import Root from "./components/Root";
-import Animes from "./components/Animes";
 import NotFound from "./routes/NotFound";
-import Seasonanime from "./routes/Seasonanime";
+import FilterSeason from "./routes/sourcefilters/FilterSeason";
+import DjangoLogin from "./routes/DjangoLogin";
+
+import UserMe from "./routes/UserMe";
+import MyLists from "./routes/mylists/MyLists";
+import FilterGenre from "./routes/sourcefilters/FilterGenre";
+import FilterSource from "./routes/sourcefilters/FilterSource";
+import FilterStudio from "./routes/sourcefilters/FilterStudio";
+import MyListDetail from "./routes/mylists/MyListDetail";
 
 const router = createBrowserRouter([
   {
@@ -18,12 +25,37 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: "anime/season",
-        element: <Seasonanime />,
+        path: "seasons/:seasonPk/animes",
+        element: <FilterSeason />,
+      },
+
+      {
+        path: "genres/:genrePk/animes",
+        element: <FilterGenre />,
       },
       {
-        path: "animes",
-        element: <Animes />,
+        path: "studios/:studioPk/animes",
+        element: <FilterStudio />,
+      },
+      {
+        path: "sources/:sourcePk/animes",
+        element: <FilterSource />,
+      },
+      {
+        path: "login",
+        element: <DjangoLogin />,
+      },
+      {
+        path: "users/me",
+        element: <UserMe />,
+      },
+      {
+        path: "mylists",
+        element: <MyLists />,
+      },
+      {
+        path: "mylists/:mylistPk",
+        element: <MyListDetail />,
       },
     ],
   },
